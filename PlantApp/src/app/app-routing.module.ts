b6dfa6cf777811c4fe32ac8ee9da0e311b6b061d
Sagittare <e.angevare@st.hanze.nl>
2021-06-15 19:41:11 +0200

    components

--- PlantApp/src/app/app-routing.module.ts
+++ PlantApp/src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './auth-guard.service';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { TileComponent } from './tile/tile.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   {path: '', children: [
-    {path: 'main', component: MainComponent},
+    {path: 'main', 
+    component: MainComponent,
+    children: [
+      {path: 'start', component: TileComponent}
+    ]},
     {path: 'login', component: LoginComponent},
     {path: 'settings',
-    component: SettingsComponent//,
-    //canActivate: [AuthGuardService]
+    component: SettingsComponent,
+    canActivate: [AuthGuardService]
     }
   ]},
   {path: '**', component: MainComponent}
