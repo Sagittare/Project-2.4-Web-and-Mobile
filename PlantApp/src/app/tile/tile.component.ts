b4f398ee1a459ad5b15d22d5af60a9ceca3140ce
Daniël Hofman <danielhofman@ziggo.nl>
2021-06-22 20:19:34 +0200

    Tile component

--- PlantApp/src/app/tile/tile.component.ts
+++ PlantApp/src/app/tile/tile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
-  styleUrls: ['../app.component.css']
+  styleUrls: ['./tile.component.css']
 })
 export class TileComponent implements OnInit {
 
