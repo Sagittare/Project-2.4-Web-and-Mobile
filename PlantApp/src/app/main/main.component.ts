1a33178d5dca9dec2dea3c7631652d90f1770e06
Wouter50 <wo.de.boer@st.hanze.nl>
2021-06-23 15:49:39 +0200

    begin notifications

--- PlantApp/src/app/main/main.component.ts
+++ PlantApp/src/app/main/main.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+//import * as fs from 'file-system';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
+  private readonly publicKey = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEzCFrALch4sArU0psCD5XPVbU9ExtE+LFYiVg9LnXLRH6lL4XQBCPNbRZWPN4+uoYzyxupU6HZM5nNbDFK5opyg==";
 
-  constructor() { }
+  constructor(
+    private swPush: SwPush
+  ) { }
+  
 
-  ngOnInit(): void {
+  subscribeNotifications(){
+    if (!this.swPush.isEnabled){
+      console.log("Notifications not enabled")
+      return;
+    }
+    this.swPush.requestSubscription({
+      serverPublicKey: this.publicKey
+    })
+      .then(sub=> {
+
+        console.log("pog", sub)
+      })
+      .catch(err => console.error("antipog", err))
   }
+
 
 }
