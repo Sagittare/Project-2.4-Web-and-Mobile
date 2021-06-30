9bcc3858f440222e5e53ead53680c92a8cdbc0a4
Daniël Hofman <danielhofman@ziggo.nl>
2021-06-30 23:23:56 +0200

    Gimme Your Plant

--- PlantApp/src/app/grid-assembler/grid-assembler.component.ts
+++ PlantApp/src/app/grid-assembler/grid-assembler.component.ts
@@ -15,11 +15,26 @@ export class GridAssemblerComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    //this.generateTiles();
   }
+
   public getData() {
     const getPlants = this.dataService.getUserPlants(this.authService.getToken());
     getPlants.subscribe(res => {
       console.log(JSON.stringify(res))
     })
   }
+
+  generateTiles() {
+    let tiles = document.getElementById("tiles");
+    let output = "";
+    let len = 15;
+
+    tiles.innerHTML = "";
+
+    for (let i = 0; i < len; i++) {
+      tiles.innerHTML += "<p><app-tile></app-tile></p>" + "<br>";
+    }
+  }
+
 }
